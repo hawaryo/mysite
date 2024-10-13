98d12e850f4caf607f533af35b9048a329d97d4f
Ahmed <63455817+hawaryo@users.noreply.github.com>
2024-10-13 16:09:05 +0300

    feature:adding nav bar

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
+import styles from "./home.module.css";
 export default function Home() {
   return (
     <>
-      <div>
-        <h1>Home</h1>
-      </div>
+      <nav className={styles.navbar}>
+        <a href="/projects">Projects</a>
+        <Link href="/projects">Articles</Link>
+        <a href="/projects">Contact</a>
+        <a href="/projects">Github</a>
+        <a href="/projects">CV</a>
+      </nav>
     </>
   );
 }
