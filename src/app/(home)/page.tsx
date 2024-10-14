554fc1c69aa6ab70312efcc193e63213ae0c0fe1
Ahmed <63455817+hawaryo@users.noreply.github.com>
2024-10-14 14:38:55 +0300

    feature: adding the middle section of the home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import styles from "./home.module.css";
+import Image from "next/image";
 export default function Home() {
   return (
     <>
@@ -10,6 +11,42 @@ export default function Home() {
         <a href="/projects">Github</a>
         <a href="/projects">CV</a>
       </nav>
+      <h1 className={styles.title}>
+        Front-end developer crafting user-friendly, accessible, and engaging
+        websites
+      </h1>
+      <section className={styles.logosbar}>
+        <Image
+          src="/images/html5-logo.svg"
+          alt="html logo"
+          width={74}
+          height={74}
+        />
+        <Image
+          src="/images/css-3-logo.svg"
+          alt="css logo"
+          width={73}
+          height={73}
+        />
+        <Image
+          src="/images/typescript-logo.svg"
+          alt="type scrpit logo"
+          width={73}
+          height={74}
+        />
+        <Image
+          src="/images/react-logo.svg"
+          alt="react logo"
+          width={74}
+          height={74}
+        />
+        <Image
+          src="/images/nextjs-logo.svg"
+          alt="nextjs logo"
+          width={63}
+          height={65}
+        />
+      </section>
     </>
   );
 }
